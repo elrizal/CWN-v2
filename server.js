4162182b3a8710cec9afeb5b5fa46aa6876e99df
elrizal <erin@lrizal.com>
2019-07-17 23:51:59 -0400

    setting up some dependencies

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const express = require("express");
-const listEndpoints = require("express-list-endpoints");
 const bodyParser = require("body-parser");
 const session = require("express-session");
-const routes = require("./routes");
-const path = require("path");
+//const routes = require("./routes");
 const app = express();
 const morgan = require("morgan");
 
-const PORT = process.env.PORT || 8080;
-const db = require("./models");
+const PORT = process.env.PORT || 7000;
+//const db = require("./models");
+
+//const passport = require("./config/passport");
+
+app.use(morgan());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/test',(req,res) => {
+  if (req.user) return res.send({Hi:"There"});
+  res.send({message: 'You\' not suppose to be here you filthy animal!'})
+});
+
+app.use(routes);
+
+
+db.sequelize.sync({ force: false }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
